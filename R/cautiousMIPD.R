9578a06571cb5d19cdd58c13e07c7c2f805f2e3a
luyckxn <nicolas.luyckx@calvagone.com>
2023-04-12 14:55:56 +0200

    #6 Review current implementation.

--- R/cautiousMIPD.R
+++ R/cautiousMIPD.R
@@ -1,77 +1,3 @@
-#' @describeIn doseSimulation Find the dose required to hit a target.
-#'
-#' @param fit the tdmorefit object
-#' @param regimen the regimen
-#' @param doseRows which rows of the regimen to adapt when searching for a new dose, or NULL to take the last one
-#' @param interval which interval to search a dose in. Defaults to a ridiculously high range
-#' @param target target value, as a data.frame
-#' @param ... extra arguments passed to stats::uniroot
-#'
-#' @return a recommendation object
-#' @export
-#' @importFrom stats uniroot
-findDoseCautiously <- function(fit, regimen=fit$regimen, doseRows=NULL, interval=c(0, 1E10), target, ...) {
-  if(is.null(doseRows))
-    doseRows <- nrow(regimen)
-
-  if(nrow(target) > 1) {
-    stop("Cannot find the dose to hit multiple targets. Split the treatment regimen and perform findDose separately per target.")
-  }
-  if(ncol(target[, colnames(target) != "TIME", drop=FALSE]) > 1) {
-    stop("Cannot find the dose to hit multiple targets. Split the treatment regimen and perform findDose separately per target.")
-  }
-
-  # Find the best dose for the estimated parameters
-  rootFunction <- function(AMT) {
-    myRegimen <- updateRegimen(regimen = regimen, doseRows = doseRows, newDose = AMT)
-    obs <- stats::predict(fit, newdata = target, regimen = myRegimen)
-    result <- obs[, colnames(obs) != "TIME", drop=TRUE] - target[, colnames(target) != "TIME", drop=TRUE]
-    if(length(result) > 1) stop("Cannot use findDose to hit multiple targets!")
-    result
-  }
-  result <- runUniroot(rootFunction, interval, ...)
-  return(convertResultToRecommendation(fit, result, regimen, doseRows, target))
-}
-
-runUniroot <- function(rootFunction, interval, ...) {
-  iValues <- c(rootFunction(interval[1]), rootFunction(interval[2]))
-
-  if(sign(iValues[1]) == sign(iValues[2])) {
-    warning("Predicted values at edges of interval both ",
-            switch(sign(iValues[1]), "0"="equal to", "1"="above", "2"="below"),
-            " target, returning closest value...")
-    i <- which.min(abs(iValues))
-
-    result <- list(
-      root=interval[i],
-      f.root=iValues[i],
-      iter=0,
-      estim.prec=Inf
-    )
-    return(result)
-  } else {
-    return(uniroot(f=rootFunction, interval=interval, ...))
-  }
-}
-
-#' Update a regimen with the specified dose.
-#'
-#' @param regimen the regimen to update
-#' @param doseRows which rows of the regimen to adapt, if not specified, the last dose will be adapted
-#' @param newDose the specified new dose
-#'
-#' @return the updated regimen
-#' @noRd
-updateRegimen <- function(regimen, doseRows = NULL, newDose) {
-  if (is.null(doseRows))
-    doseRows <- nrow(regimen)
-
-  dose <- numeric(length = length(doseRows)) + as.numeric(newDose)
-  names(dose) <- paste0(doseRows, ".AMT")
-  updatedRegimen <- transformRegimen(regimen, dose)
-
-  return(updatedRegimen)
-}
 
 #' @describeIn doseSimulation Optimize the regimen, using the metadata defined by the model
 #'
@@ -82,14 +8,14 @@ updateRegimen <- function(regimen, doseRows = NULL, newDose) {
 #' @param targetMetadata defined target troughs as list(min=X, max=Y). If NULL or all NA, taken from the model metadata.
 #'
 #' @export
-findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL, smoother=NULL) {
+findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL) {
   if(! "FIX" %in% colnames(regimen) ) regimen$FIX <- FALSE
   if(is.null(targetMetadata) || all(is.na(targetMetadata))) {
     targetMetadata <- tdmore::getMetadataByClass(fit$tdmore, "tdmore_target")
     if(is.null(targetMetadata)) stop("No target defined in model metadata")
   }
   stopifnot( all( c("min", "max") %in% names(targetMetadata) ) )
-  stopifnot( is.integer(smoother) && smoother >= 0L )
+  # stopifnot( is.integer(smoother) && smoother >= 0L )
 
   obsConc <- fit$observed$CONC
 
@@ -97,9 +23,10 @@ findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL, s
   lastConc <- obsConc[obsConc %>% length()]
 
   if (lastConc < 12.5) {
-    smoother <- 1L
+    # smoother <- 0L # target=morning dose trough of current occasion
+    smoother <- 1L   # target=morning dose trough of next occasion
   } else {
-    smoother <- 0L
+    smoother <- NULL # Free-style
   }
 
   # Unfixed part of regimen
@@ -107,7 +34,7 @@ findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL, s
     dplyr::mutate(ROW_NO=dplyr::row_number()) %>%
     dplyr::filter(!FIX)
 
-  if (nrow(unfixedRegimen) > 0) {
+  if (nrow(unfixedRegimen) > 0 && !is.null(smoother)) {
     currentOcc <- unfixedRegimen %>% dplyr::pull(OCC) %>% dplyr::first()
     occasions <- unfixedRegimen$OCC
     # Find latest occasion available from data
@@ -122,7 +49,7 @@ findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL, s
     )
   } else {
     target <- list(
-      TIME=numeric(0)
+      TIME=getTroughs(fit$tdmore, regimen[regimen$FIX==FALSE, ], adj=TRUE)
     )
   }
 
@@ -146,10 +73,15 @@ findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL, s
       form <- tdmore::getMetadataByName(fit$tdmore, form)
       form$round_function(amt)
     })
-    iRoundedAmt <- roundedAmt[iterationRows] #rounded amounts only
-    maxAmount <- 20000*(smoother + 1)
-    if (sum(iRoundedAmt) > maxAmount) {
-      iRoundedAmt <- rep(maxAmount/(iRoundedAmt %>% length()), iRoundedAmt %>% length())
+    iRoundedAmt <- roundedAmt[iterationRows] # Rounded amounts only
+
+    # Capped dose when smoother is used (max 20mg/occasion)
+    # Number of occasions = smoother + 1
+    if (!is.null(smoother)) {
+      maxAmount <- 20000*(smoother + 1)
+      if (sum(iRoundedAmt) > maxAmount) {
+        iRoundedAmt <- rep(maxAmount/(iRoundedAmt %>% length()), iRoundedAmt %>% length())
+      }
     }
     regimen$AMT[iterationRows] <- iRoundedAmt
     modified[ iterationRows ] <- TRUE
