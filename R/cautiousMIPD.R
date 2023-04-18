b0ac904e4781ac74dcd0ee19499bfc31533bbf40
luyckxn <nicolas.luyckx@calvagone.com>
2023-04-18 16:58:50 +0200

    #6 doseCap and foldFactor as arguments.

--- R/cautiousMIPD.R
+++ R/cautiousMIPD.R
@@ -31,25 +31,27 @@ getMaxAmountFromPast <-function(regimen, fit) {
 #' @param fit tdmorefit object
 #' @param regimen the treatment regimen to optimize
 #' @param targetMetadata defined target troughs as list(min=X, max=Y). If NULL or all NA, taken from the model metadata.
-#'
+#' @param doseCap dose cap that cannot be exceeded (unit: prograf dose)
+#' @param foldFactor fold factor threshold (recommended doses compared with max dose already given)
 #' @export
-findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL) {
+findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL, doseCap, foldFactor) {
   if(! "FIX" %in% colnames(regimen) ) regimen$FIX <- FALSE
   if(is.null(targetMetadata) || all(is.na(targetMetadata))) {
     targetMetadata <- tdmore::getMetadataByClass(fit$tdmore, "tdmore_target")
     if(is.null(targetMetadata)) stop("No target defined in model metadata")
   }
   stopifnot( all( c("min", "max") %in% names(targetMetadata) ) )
-  # stopifnot( is.integer(smoother) && smoother >= 0L )
 
-  doseCap <- 9000
-  ff <- 1.5
+  # Retrieving target value
+  targetValue <- mean( c(targetMetadata$min, targetMetadata$max) )
+  if(is.na(targetValue)) stop("Target not defined, cannot optimize treatment...")
+
   obsConc <- fit$observed$CONC
 
   # Retrieving the last observed concentration
   lastConc <- obsConc[obsConc %>% length()]
 
-  if (lastConc < 12.5) {
+  if (lastConc < targetValue) {
     # smoother <- 0L # target=morning dose trough of current occasion
     smoother <- 1L   # target=morning dose trough of next occasion
   } else {
@@ -81,8 +83,6 @@ findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL) {
   }
 
   outputVar <- fit$tdmore$res_var[[1]]$var
-  targetValue <- mean( c(targetMetadata$min, targetMetadata$max) )
-  if(is.na(targetValue)) stop("Target not defined, cannot optimize treatment...")
   target[outputVar] <- targetValue
   target <- tibble::as_tibble(target)
   print(target)
@@ -93,7 +93,7 @@ findDosesCautiously <- function(fit, regimen=fit$regimen, targetMetadata=NULL) {
   for(i in seq_along(target$TIME)) {
     row <- target[i, ]
     iterationRows <- which( regimen$FIX == FALSE & regimen$TIME < row$TIME & !modified )
-    maxAllowedAmount <- getMaxAmountFromPast(regimen=regimen, fit=fit)*ff
+    maxAllowedAmount <- getMaxAmountFromPast(regimen=regimen, fit=fit)*foldFactor
     if(maxAllowedAmount > doseCap) {
       maxAllowedAmount <- doseCap # Cannot exceed dose cap
     }
